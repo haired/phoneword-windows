c51874071e641a58aaeb6d7dd6c478cf022a5602
Didier Gohourou <godidier@godidier.com>
2015-03-30 15:48:22 +0000

    Some page transitions added, manifest file updated

--- PhoneWord/Properties/AssemblyInfo.cs
+++ PhoneWord/Properties/AssemblyInfo.cs
@@ -7,9 +7,9 @@ using System.Resources;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("PhoneWord")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Convert Your phone number to letters")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("GoDidier")]
 [assembly: AssemblyProduct("PhoneWord")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
@@ -32,6 +32,6 @@ using System.Resources;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.7.0")]
+[assembly: AssemblyFileVersion("0.7.0")]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
